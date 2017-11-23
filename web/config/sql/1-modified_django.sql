7cef5cd2b1df9971ec8886c12faeffd5bae76068
陆彬 <bbyan2006@live.cn>
2017-11-23 21:37:02 +0800

    在docker-compose中添加container_name，修改1-modified_django.sql，使其能够应对视图已创建的情况

--- web/config/sql/1-modified_django.sql
+++ web/config/sql/1-modified_django.sql
@@ -1,5 +1,5 @@
 use django;
-CREATE ALGORITHM=UNDEFINED DEFINER=`django`@`%` SQL SECURITY DEFINER VIEW `zabbix_temp_for_issue_trigger_name`
+CREATE OR REPLACE VIEW  `zabbix_temp_for_issue_trigger_name`
 AS SELECT
    `zabbix_trigger_status`.`rowcreatedatetime` AS `rowcreatedatetime`,
    `zabbix_trigger_status`.`rowoperatedatetime` AS `rowoperatedatetime`,
@@ -10,12 +10,12 @@ AS SELECT
    `zabbix_trigger_status`.`applicationname` AS `applicationname`,
    `zabbix_trigger_status`.`hostname` AS `hostname`
 FROM `zabbix_trigger_status` where ((`zabbix_trigger_status`.`applicationname` <> 'HOST') and (`zabbix_trigger_status`.`triggervalue` = '1')) order by `zabbix_trigger_status`.`rowoperatedatetime`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`django`@`%` SQL SECURITY DEFINER VIEW `zabbix_issue_trigger_name`
+CREATE OR REPLACE VIEW  `zabbix_issue_trigger_name`
 AS SELECT
    `zabbix_temp_for_issue_trigger_name`.`applicationname` AS `applicationname`,
    `zabbix_temp_for_issue_trigger_name`.`triggername` AS `triggername`
 FROM `zabbix_temp_for_issue_trigger_name` group by `zabbix_temp_for_issue_trigger_name`.`applicationname`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`django`@`%` SQL SECURITY DEFINER VIEW `zabbix_subsystemname`
+CREATE OR REPLACE VIEW `zabbix_subsystemname`
 AS SELECT
    `a`.`applicationname` AS `applicationname`,sum(`a`.`triggervalue`) AS `triggervalue`,
    `b`.`triggername` AS `triggername`
